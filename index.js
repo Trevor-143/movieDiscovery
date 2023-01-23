f86fee89fb1f1af37df623d1c967438a7baa2d8e
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-01-23 19:00:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const key = 'ad6fb9de597140d6888525b8855e22b1';
 const imgApi = 'https://image.tmdb.org/3/t/p/w1280';
+const search_text = document.getElementById('search_text');
+const btn = document.getElementById('btn');
+let ressSpan = document.querySelector('.ressSpan');
 
 let upMovies = '';
 let popMovies = '';
 let topMovies = '';
+let foundMovies = '';
 
 //for the upcoming
 function getUpcomingMovies() {
@@ -13,7 +17,7 @@ function getUpcomingMovies() {
         // console.log(data)
         data.results.forEach(movie => {
             upMovies += `<li>
-                            <img src="${imgApi}${movie.backdrop_path}" alt="" >
+                            <img src="${imgApi}${movie.poster_path}" alt="" >
                             <div>
                                 <p>${movie.overview}</p>
                                 <h3>${movie.title}</h3>
@@ -36,7 +40,7 @@ function getPopularMovies() {
         // console.log(data)
         data.results.forEach(movie => {
             popMovies += `<li>
-                            <img src="${imgApi}${movie.backdrop_path}" alt="" >
+                            <img src="${imgApi}${movie.poster_path}" alt="" >
                             <div>
                                 <p>${movie.overview}</p>
                                 <h3>${movie.title}</h3>
@@ -61,7 +65,7 @@ function getTopRatedMovies() {
         // console.log(data)
         data.results.forEach(movie => {
             topMovies += `<li>
-                            <img src="${imgApi}${movie.backdrop_path}" alt="" >
+                            <img src="${imgApi}${movie.poster_path}" alt="" >
                             <div>
                                 <p>${movie.overview}</p>
                                 <h3>${movie.title}</h3>
@@ -76,6 +80,44 @@ function getTopRatedMovies() {
     });
 }
 
+function getSearched() {
+    fetch(`https://api.themoviedb.org/3/search/movie?&api_key=${key}&query=${search_text.value}`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        data.results.forEach(movie => {
+            foundMovies += `<li class="movFound">
+                                <img class="poster" src="${imgApi}${movie.poster_path}" alt="" >
+                                <img class="backdrop" src="${imgApi}${movie.backdrop_path}" alt="" >
+                                <div>
+                                    <p>${movie.overview}</p>
+                                    <h3>${movie.title}</h3>
+                                    <h4>${movie.release_date}</h4>
+                                    <h5>${movie.vote_average}</h5>
+                                </div>
+                            </li>`
+
+            document.getElementById('foundMovies').innerHTML = foundMovies;
+            console.log(movie)
+        
+        })
+    });
+}
+ressSpan.innerHTML = ''
+
+btn.addEventListener('click', (e) => {
+    foundMovies = '';
+    e.preventDefault()
+    if (search_text.value !== null ) {
+        ressSpan.innerHTML = `<span class="ress">Your search results</span> 
+                            <i class="fa-solid fa-chevron-left rel"></i>
+                            <i class="fa-solid fa-chevron-right rer"></i>`
+        getSearched();
+    } else {
+        ressSpan.innerHTML = '';
+    }
+});
+
 getUpcomingMovies();
 getPopularMovies();
 getTopRatedMovies();
@@ -84,6 +126,7 @@ getTopRatedMovies();
 const upcomingMovies = document.getElementById('upMovies');
 const popularMovies = document.getElementById('popMovies');
 const topRatedMovies = document.getElementById('topMovies');
+const gotMovies = document.getElementById('foundMovies');
 
 const upl = document.querySelector('.upl');
 const upr = document.querySelector('.upr');
@@ -91,6 +134,8 @@ const popl = document.querySelector('.popl');
 const popr = document.querySelector('.popr');
 const topl = document.querySelector('.topl');
 const topr = document.querySelector('.topr');
+const rel = document.querySelector('.rel');
+const rer = document.querySelector('.rer');
 
 upl.addEventListener('click', () => {
     upcomingMovies.scrollLeft -= 400;
@@ -109,4 +154,10 @@ topl.addEventListener('click', () => {
 })
 topr.addEventListener('click', () => {
     topRatedMovies.scrollLeft += 400;
+})
+rel.addEventListener('click', () => {
+    gotMovies.scrollLeft -= 400;
+})
+rer.addEventListener('click', () => {
+    gotMovies.scrollLeft += 400;
 })
